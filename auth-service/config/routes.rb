61265ba445fe00f120a34885fe08fcf635a183ed
CC9537 <clint.chancellor@gmail.com>
2021-01-16 19:01:33 -0600

    readd auth service

---
+++ auth-service/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+#  namespace :api, defaults: { format: :json } do
+#    namespace :v1 do
+      resources :users, only: %w[show]
+#    end
+#  end
+
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: '/login',
+      sign_out: '/logout',
+      registration: '/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
+end
